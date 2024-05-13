df0710ac355d983fcb4d4bc041c255e66d6e3627
Oleh Panchenko <106483317+opanchen@users.noreply.github.com>
2024-05-13 12:03:31 +0200

    create CreateServerModal & modal-provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { ClerkProvider } from "@clerk/nextjs";
 import { cn } from "@/lib/utils";
 
 import { ThemeProvider } from "@/components/providers/theme-provider";
+import { ModalProvider } from "@/components/providers/modal-provider";
 
 import "./globals.css";
 
@@ -30,6 +31,7 @@ export default function RootLayout({
             enableSystem={false}
             storageKey="discord-theme"
           >
+            <ModalProvider />
             {children}
           </ThemeProvider>
         </body>
