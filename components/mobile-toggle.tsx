51e20b05971613204755cadd1b36bf008466919b
Oleh Panchenko <106483317+opanchen@users.noreply.github.com>
2024-05-15 10:38:15 +0200

    upd prisma schema

--- components/mobile-toggle.tsx
+++ components/mobile-toggle.tsx
@@ -16,11 +16,11 @@ export const MobileToggle = ({ serverId }: { serverId: string }) => {
 
       <SheetContent side="left" className="p-0 flex gap-0">
         <div className="w-[72px]">
-          //! server component inside - this approach can be wrong
+          {/* //! server component inside - this approach can be wrong */}
           {/* @ts-expect-error Server Component */}
           <NavigationSidebar />
         </div>
-        //! server component inside - this approach can be wrong
+        {/* //! server component inside - this approach can be wrong */}
         {/* @ts-expect-error Server Component */}
         <ServerSidebar serverId={serverId} />
       </SheetContent>
