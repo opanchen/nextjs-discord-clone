182a7ca05e779e2853910a45410ddddfe1a7da49
Oleh Panchenko <106483317+opanchen@users.noreply.github.com>
2024-05-15 17:30:49 +0200

    init messages api

---
+++ lib/current-profile-pages.ts
@@ -0,0 +1,20 @@
+// special version of current-profile.ts for 'pages' directory
+
+import { NextApiRequest } from "next";
+import { getAuth } from "@clerk/nextjs/server";
+
+import { db } from "@/lib/db";
+
+export const currentProfilePages = async (req: NextApiRequest) => {
+  const { userId } = getAuth(req);
+
+  if (!userId) return null;
+
+  const profile = await db.profile.findUnique({
+    where: {
+      userId,
+    },
+  });
+
+  return profile;
+};
