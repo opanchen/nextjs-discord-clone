8c868b1ad43bde19fe41bbfbaea2e68e74b39bdb
Oleh Panchenko <106483317+opanchen@users.noreply.github.com>
2024-05-17 10:51:58 +0200

    upd mobile-menu

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -37,9 +37,9 @@ export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger className="focus:outline-none" asChild>
-        <button className="w-full text-md font-semibold px-3 flex items-center h-12 border-neutral-200 dark:border-neutral-800 border-b-2 hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition">
+        <button className="group pr-[15%] w-full text-md  md:text-md font-semibold px-3 flex items-center h-12 border-neutral-200 dark:border-neutral-800 border-b-2 hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition">
           {server.name}
-          <ChevronDown className="h-5 w-5 ml-auto" />
+          <ChevronDown className="h-5 w-5 ml-auto group-data-[state=open]:rotate-180" />
         </button>
       </DropdownMenuTrigger>
 
